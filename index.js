24737daee82bd2eddef066968536ec002eb88c19
Max Nikitin <fugazister@gmail.com>
2024-06-26 18:29:22 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const http = require('node:http');
+const https = require('node:https');
+const { parse } = require('node-html-parser');
+const BASE_URL = 'https://tonsky.me';
+const vigenere = require('vigenere');
+
+function get(url, successCallback, errorCallback) {
+	https.get(url, (res) => {
+		let data = [];
+		let response;
+	
+		res.on('data', chunk => {
+			data.push(chunk);
+		});
+	
+		res.on('end', () => {
+			response = Buffer.concat(data).toString();
+			successCallback(response);
+		});
+	}).on('error', (error) => {
+		errorCallback(error);
+	})
+}
+
+function theRemover(domString) {
+	const root = parse(domString);
+  
+	[
+		...root.querySelectorAll('.dark_mode'),
+		...root.querySelectorAll('.cover'),
+		...root.querySelectorAll('.spacer'),
+		...root.querySelectorAll('.pointers'),
+		...root.querySelectorAll('script'),
+		...root.querySelectorAll('img'),
+		...root.querySelectorAll('meta'),
+		...root.querySelectorAll('link'),
+		...root.querySelectorAll('video'),
+	].forEach(el => el.remove());
+
+	root.querySelectorAll('a').forEach(link => {
+		let href = link.getAttribute('href');
+
+		if (href.startsWith('/')) {
+			link.setAttribute('href', `${BASE_URL}${href}`);
+		}
+	});
+
+	const result = root.toString();
+
+	return result;
+}
+
+function theCipher(input) {
+	return vigenere.encode(input, 'nikitonsky').split('').join(' ');
+}
+
+function thePackager(input) {
+	const parsedInput = input;
+	const result = `
+	<html>
+		<head>
+			<title>btonsky.me</title>
+		</head>
+		<body style="font-family: monospace; font-size: large; text-transform: uppercase">${parsedInput}</body>
+	</html>`;
+
+	return result;
+}
+
+const server = http.createServer((req, res) => {
+	if (req.url.includes('favicon')) {
+		res.writeHead(404);
+		return res.end();
+	}
+
+	get(`${BASE_URL}${req.url}`, (response) => {
+		res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+		res.end(thePackager(theCipher(theRemover(response))));
+	}, () => {
+		res.writeHead(418);
+		res.end();
+	});
+});
+
+if (typeof process.env.PORT === 'undefined') {
+	console.error('PORT environment variable must be set');
+} else {
+	server.listen(process.env.PORT);
+}
